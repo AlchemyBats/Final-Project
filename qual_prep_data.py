a75411b6d24cfb2ae4de432f6ae1ebd951e124f8
AlchemyBats <118030315+AlchemyBats@users.noreply.github.com>
2025-12-08 16:36:59 -0600

    Delete qual_prep_data.py

--- qual_prep_data.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("reports/10q25a.pdf") as pdf, open("reports_formatted/output.txt", "w", encoding="utf-8") as f:
-    
-    for page in pdf.pages:
-        t = page.extract_text()
-        if t:
-            f.write(t + '\n')
